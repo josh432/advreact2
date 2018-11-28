03bebbecda5fd74e56dbd378f836f41c3c231d29
Josh Silverman <jrsilverman71@hotmail.com>
2018-11-28 12:11:33 -0800

    form submittting item and image hosting cloudinary

--- frontend/components/CreateItem.js
+++ frontend/components/CreateItem.js
@@ -1,10 +1,10 @@
 import React, { Component } from "react";
 import { Mutation } from "react-apollo";
 import gql from "graphql-tag";
-import Router from 'next/router';
+import Router from "next/router";
 import Form from "./styles/Form";
 import formatMoney from "../lib/formatMoney";
-import Error from './ErrorMessage';
+import Error from "./ErrorMessage";
 
 const CREATE_ITEM_MUTATION = gql`
   mutation CREATE_ITEM_MUTATION(
@@ -40,25 +40,60 @@ class CreateItem extends Component {
     const val = type === "number" ? parseFloat(value) : value;
     this.setState({ [name]: val });
   };
+
+  uploadFile = async e => {
+    console.log("uploading file...");
+    const files = e.target.files;
+    const data = new FormData();
+    data.append("file", files[0]);
+    data.append("upload_preset", "sickfits");
+
+    const res = await fetch(
+      "https://api.cloudinary.com/v1_1/josh432/image/upload",
+      {
+        method: "POST",
+        body: data
+      }
+    );
+    const file = await res.json();
+    console.log(file);
+    this.setState({
+      image: file.secure_url,
+      largeImage: file.eager[0].secure_url
+    });
+  };
   render() {
     return (
       <Mutation mutation={CREATE_ITEM_MUTATION} variables={this.state}>
         {(createItem, { loading, error }) => (
           <Form
-          //stop the form from submitting
+            //stop the form from submitting
             onSubmit={async e => {
               e.preventDefault();
-           //call the mutation   
+              //call the mutation
               const res = await createItem();
-            //change them to the single item page
+              //change them to the single item page
               Router.push({
-                  pathname: '/item',
-                  query: { id: res.data.createItem.id }
+                pathname: "/item",
+                query: { id: res.data.createItem.id }
               });
             }}
           >
-          <Error error={error} />
+            <Error error={error} />
             <fieldset disabled={loading} aria-busy={loading}>
+              <label htmlFor="file">
+                Image
+                <input
+                  type="file"
+                  id="file"
+                  name="file"
+                  placeholder="Upload an image"
+                  required
+                  onChange={this.uploadFile}
+                />
+                {this.state.image && <img width="200" src={this.state.image} alt="Upload Preview" />}
+              </label>
+
               <label htmlFor="title">
                 Title
                 <input
