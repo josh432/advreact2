896adfe591a4d49debd1bfc2e73a7eb540eca05d
Josh Silverman <jrsilverman71@hotmail.com>
2018-12-17 09:37:10 -0800

    signin component built

--- frontend/pages/sell.js
+++ frontend/pages/sell.js
@@ -1,8 +1,10 @@
 import CreateItem from '../components/CreateItem'
+import PleaseSignIn from '../components/PleaseSignin';
 const Sell = props => (
     <div>
+       <PleaseSignIn>
         <CreateItem />
-        
+        </PleaseSignIn>   
     </div>
 );
 export default Sell;
