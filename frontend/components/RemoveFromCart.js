855e3173200a014eee97742946a0f67459966c9e
Josh Silverman <jrsilverman71@hotmail.com>
2019-01-14 10:55:42 -0800

    remove from item optimistic response

--- frontend/components/RemoveFromCart.js
+++ frontend/components/RemoveFromCart.js
@@ -28,10 +28,31 @@ class RemoveFromCart extends React.Component {
     id: PropTypes.string.isRequired
   };
 
+  //This gets called as soon as we get a response back from the server after a mutation has been performed
+
+  update = (cache, payload) => {
+      //1. First read the cache
+      console.log('Running remove from cart update fn');
+      const data = cache.readQuery({
+          query: CURRENT_USER_QUERY
+      });
+      console.log(data);
+      //2. Remove that item from the cart
+      const cartItemId = payload.data.removeFromCart.id;
+      data.me.cart = data.me.cart.filter(cartItem => cartItem.id !== cartItemId);
+      //3. Write it back to the cache
+      cache.writeQuery({ query: CURRENT_USER_QUERY, data })
+  }
+
   render() {
     return (
       <Mutation mutation={REMOVE_FROM_CART_MUTATION}
-      variables={{id: this.props.id}}>
+      variables={{id: this.props.id}} update={this.update} optimisticResponse={{
+          __typename: 'Mutation', removeFromCart: {
+              __typename: 'CartItem',
+              id: this.props.id,
+          },
+      }}>
         {(removeFromCart, { loading, error }) => (
           <BigButton
             disabled={loading}
